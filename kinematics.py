d0031359c7c79324bb0e0c279d5de43c74df7fec
Martijn Brummelhuis <mbrummelhuis@gmail.com>
2024-07-18 22:30:11 +0200

    [DEV] Added inverse kinematics
    Implemented inverse kinematics calculation through newton-raphson method (ref modern robotics)

--- kinematics.py
+++ kinematics.py
@@ -13,6 +13,8 @@ class KinematicsSolver:
         Args:
             transformation (Sympy Expr): 4x4 homogeneous transformation matrix describing the 
                                          end-effector pose in the world frame
+            jacobian (Sympy Expr): 6xN jacobian matrix of the aerial manipulator with N the
+                                   total degrees of freedom
         """
         self.transformation_matrix = transformation
 
@@ -21,11 +23,11 @@ class KinematicsSolver:
         self.joint_variables = self.transformation_matrix.free_symbols
         self.dofs = len(self.joint_variables)
 
-    def __forwardKinematics(self, configuration: list):
+    def __forwardKinematics(self, configuration: np.array):
         """Forward kinematics function
 
         Args:
-            configuration (list): List of desired joint positions
+            configuration (Numpy array): List of desired joint positions
 
         Returns:
             list: List with full pose result
@@ -49,25 +51,80 @@ class KinematicsSolver:
         result[5] = sp.atan2(self.transformation_matrix[1,0].subs(joint_commands), self.transformation_matrix[0,0].subs(joint_commands))
         return result
 
-    def __inverseKinematics(self, pose: list):
+    def __inverseKinematics(self, target_pose: np.array, initial_guess: np.array = None, tolerance: float = 1e-6):
         """Inverse Kinematics function
 
         Args:
-            pose (list): Desired end-effector pose
+            pose (Numpy array): Desired end-effector pose
+        
+        Returns:
+            result (Numpy array): Configuration resulting in the target pose
         """
         # Check if analytical jacobian is provided
         if self.analytical_jacobian is None:
             print("[KINEMATICS SOLVER] Error: Please provide the analytical Jacobian")
             return
 
-        # Implement the inverse kinematics using Newton-Raphson such as in Modern Robotics
+        # Initial guess
+        if initial_guess is None:
+            initial_guess = np.zeros(self.dofs)
+        
+        # Newton-Raphson implementation
+        # Initialization
+        error = target_pose - self.__forwardKinematics(initial_guess)
+        evaluated_jacobian = self.analytical_jacobian.subs(list(zip(self.joint_variables, initial_guess)))
+        previous_guess = initial_guess
+        counter = 0
 
+        # Running the algorithm
+        while np.linalg.norm(error) > tolerance:
+            pseudoinverse = evaluated_jacobian.transpose()*((evaluated_jacobian*evaluated_jacobian.transpose()).inv())
+            guess = previous_guess + pseudoinverse*error
 
-    def analyseWorkspace(self, space='jointspace'):
+            error = target_pose - self.__forwardKinematics(guess)
+            evaluated_jacobian = self.analytical_jacobian.subs(list(zip(self.joint_variables, guess)))
+
+            previous_guess = guess
+            counter+=1
+        
+        result = self.__forwardKinematics(guess)
+        
+        # Results statement
+        print(f"[KINEMATICS SOLVER] Inverse kinematics converged with error {error} in {counter} iterations")
+        print(f"[KINEMATICS SOLVER] Desired pose: {target_pose}")
+        print(f"[KINEMATICS SOLVER] Achieved pose: {result}")
+        return result
+
+    def analyseWorkspace(self,  limits: np.array, steps: int=10, space: str='jointspace'):
         if space is 'jointspace':
             # Use forward kinematics
-            print("Workspace analysis in joint space")
+            print("[KINEMATICS SOLVER] Workspace analysis in joint space")
+
+            # Build configuration space to explore
+            
+
+            # Set start time 
+            start = time.time()
+
+            # --- MAIN LOOP ---
+            steps = 10
+            data = np.zeros((6,steps**3))
+            counter = 0
+
+            print("Starting loop")
+            for q1step in tqdm(np.linspace(q1_MIN, q1_MAX, steps)):
+                for q2step in np.linspace(q2_MIN, q2_MAX, steps):
+                    for q3step in np.linspace(q3_MIN, q3_MAX, steps):
+                        res = self.__forwardKinematics()
+                        counter += 1
+
+            # Set end time
+            end = time.time()
+
+            # Display total elapsed time message
+            time_elapsed = end-start
+            print("Workspace finished calculating in ", time_elapsed, " seconds")
 
         elif space is 'cartesian':
             # Use inverse kinematics
-            print("Workspace analysis in cartesian space")
+            print("[KINEMATICS SOLVER] Workspace analysis in cartesian space")
